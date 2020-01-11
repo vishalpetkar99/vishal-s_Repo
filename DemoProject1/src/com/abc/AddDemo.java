7358fc94d96e8cae7f1d58ef2e3488f691f56146
vishalpetkar99 <59280885+vishalpetkar99@users.noreply.github.com>
2020-01-11 16:22:53 +0530

    New Code added to Add 2 numbers

---
+++ DemoProject1/src/com/abc/AddDemo.java
@@ -0,0 +1,12 @@
+package com.abc;
+
+public class AddDemo {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 20;
+		int c = a + b;
+		System.out.println("The result is " + c);
+	}
+
+}
