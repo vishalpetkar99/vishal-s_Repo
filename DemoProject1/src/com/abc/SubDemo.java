5d02e9726cea6357319ec4368a440f664ddc0212
vishalpetkar99 <59280885+vishalpetkar99@users.noreply.github.com>
2020-01-11 16:28:16 +0530

    Added new code for Sub

---
+++ DemoProject1/src/com/abc/SubDemo.java
@@ -0,0 +1,12 @@
+package com.abc;
+
+public class SubDemo {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 50;
+		int c = a-b;
+		System.out.println("The result is " + c);
+	}
+
+}
