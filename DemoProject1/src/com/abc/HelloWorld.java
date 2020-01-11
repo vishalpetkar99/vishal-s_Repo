580ba1422438bc41fa8360f1652bec78a87ba520
vishalpetkar99 <59280885+vishalpetkar99@users.noreply.github.com>
2020-01-11 16:11:36 +0530

    Project first phase

---
+++ DemoProject1/src/com/abc/HelloWorld.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.abc;
+
+/**
+ * @author Admin
+ *
+ */
+public class HelloWorld {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+
+}
